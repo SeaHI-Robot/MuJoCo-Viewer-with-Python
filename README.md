63a4b0b68c65fd7dd44f2fa4b1f7e6bbba01dfd1
Siyuan Wang <12012324@mail.sustech.edu.cn>
2024-01-23 22:55:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# A Simple MuJoCo GLFW Viewer
+
+> Based on MuJoCo Python Binding
+
+
+
+<br>
+
+
+
+## Usage
+
+- Implement user controller in `controller_implement.py`
+- Run `main.py`
---
+++ controller_implement.py
@@ -0,0 +1,14 @@
+
+xml_path = '../model_xml/hello.xml' #xml file (assumes this is in the same folder as this file)
+simend = 10 #simulation time
+
+
+def init_controller(model,data):
+    #initialize the controller here. This function is called once, in the beginning
+    pass
+
+def controller(model, data):
+    #put the controller here. This function is called inside the simulation.
+    pass
+
+
